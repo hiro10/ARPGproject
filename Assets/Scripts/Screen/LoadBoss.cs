9b1f7903f2565f5b41b8d15b3867dd7eec3330d7
hiro10 <n.t.hiro1@gmail.com>
2023-10-18 00:46:02 +0900

    1部コルーチンをUnitaskに変更

--- Assets/Scripts/Screen/LoadBoss.cs
+++ Assets/Scripts/Screen/LoadBoss.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-//TODO Unitask化
+using Cysharp.Threading.Tasks;
+
+// ボスシーンへの遷移用クラス
 public class LoadBoss : MonoBehaviour
 {
     [SerializeField] private RenderTexture renderTexture;
     private Camera renderCamera;
     [SerializeField] GameObject playerLader;
     [SerializeField] BattleSceneManager sceneManager;
-    // Start is called before the first frame update
+
     void Start()
     {
         renderCamera = Camera.main.transform.GetChild(0).GetComponent<Camera>();    //映像投影用のカメラを取得
     }
 
-    // Update is called once per frame
-    void Update()
-    {// 画面割れを始めたいタイミングに置く
-        
-    }
     public void OnClickButton()
     {
-        StartCoroutine("TraceOn");
+        LoadBossScene();
     }
-    public IEnumerator TraceOn()
+
+    private async void LoadBossScene()
+    {
+        await TraceOn();
+    }
+
+    private async UniTask TraceOn()
     {
         playerLader.SetActive(false);
         renderCamera.enabled = true;
-        renderCamera.targetTexture = renderTexture;                                 // 投影、開始
-        yield return null;                                                          // 1f待ってもらって映像をレンダーテクスチャに投影する
+        renderCamera.targetTexture = renderTexture;
+        await UniTask.Delay(500);
         renderCamera.targetTexture = null;
-        SceneManager.LoadSceneAsync("Test");                                 // シーンを遷移する
+        await SceneManager.LoadSceneAsync("Test");                                 
     }
 }
